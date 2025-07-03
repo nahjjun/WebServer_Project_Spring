94efeb5589b53d5a7d9bdc13d482c77978b41b90
나형준 <nahjjun1125@naver.com>
2025-07-03 23:57:24 +0900

    Initial commit

---
+++ src/main/java/com/example/webserver_project/WebServerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.webserver_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServerProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webserver_project/WebServerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webserver_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
