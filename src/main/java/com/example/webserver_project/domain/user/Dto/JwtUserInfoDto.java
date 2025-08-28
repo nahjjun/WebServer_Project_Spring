368ae75338a86fb40893613ecda3f5c553dd8ba4
나형준 <nahjjun1125@naver.com>
2025-08-29 00:02:37 +0900

    UserDetails, UserDetailsService 구현 완료

--- src/main/java/com/example/webserver_project/domain/user/Dto/JwtUserInfoDto.java
+++ src/main/java/com/example/webserver_project/domain/user/Dto/JwtUserInfoDto.java
@@ -3,13 +3,12 @@ package com.example.webserver_project.domain.user.Dto;
 // Jwt 로직 내부에서 인증 유저 정보를 저장해 둘 dto
 
 import com.example.webserver_project.global.jwt.RoleType;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 @Getter
-@RequiredArgsConstructor
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class JwtUserInfoDto {
     private Long userId;
     private String email;
